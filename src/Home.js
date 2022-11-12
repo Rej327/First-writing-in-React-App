5bc732a4b55c9da2595c57e3934cec35e31b4e2f
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-11-12 13:43:29 +0800

    2nd Commit {React App Writing)

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+  const title = "First Writing in React";
+  const language = "HTML, CSS and JS";
+  const fbLink = "http://www.facebook.com/resujeff27";
+
+  return (
+    <div className="Home">
+      <h1> {title} </h1>
+      <p>Implementing my {language} skills in React App.</p>
+      <p>
+        My <a href={fbLink}>Facebook account here.</a>
+      </p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Writing-React</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">Abouts Me</a>
+        <a href="/">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
