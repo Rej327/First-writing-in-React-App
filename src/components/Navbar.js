7907b485586d4525a6db71f637f529e6108fd377
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-11-15 17:47:09 +0800

    adding icon in home navigation link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HiHome } from "react-icons/hi";
 
 const Navbar = () => {
+  const style = { marginBottom: "-2px" };
+
   return (
     <div className="nav-div">
       <nav className="navbar">
         <h1>Writing-React</h1>
         <div className="links">
-          <Link to="/">Home</Link>
+          <div className="link-div">
+            <Link to="/">
+              <HiHome style={style} />
+              Home
+            </Link>
+          </div>
           <Link to="/about">About Me</Link>
           <Link to="/contact">Contact</Link>
         </div>
